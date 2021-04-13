8c06149a5c2967f9ab0aa7caf7a6cd9d6f8a06a1
Guilhem <guilhem.carlet@epita.fr>
2021-04-13 14:23:53 +0100

    Added mysql context

---
+++ Data/Context.cs
@@ -0,0 +1,11 @@
+﻿using backend_CA.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend_CA.Data
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options) { }
+        public DbSet<User> users { get; set; }
+    }
+}
